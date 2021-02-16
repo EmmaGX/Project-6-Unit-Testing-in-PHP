dca5ce4ba8cd93eb21c4238aafcc003bbc0e1dfe
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2021-02-16 17:50:56 -0500

    completes step 11

--- tests/ListingFeaturedTest.php
+++ tests/ListingFeaturedTest.php
@@ -17,4 +17,15 @@ class ListingFeaturedTest extends TestCase
         $listing->setStatus('featured');
         $this->assertEquals('featured', $listing->getStatus(), 'Did not return featured');
     }
+    /**
+     * @test
+     */
+    public function checksToSeeIfCocIsReturnsTheExpectedResults()
+    {
+        $data = ['id' => 1, 'title' => 'test'];
+        $listing = new ListingFeatured($data);
+        $listing->setCoc('5,000');
+        $this->assertIsString($listing->getCoc());
+
+    }
 }
