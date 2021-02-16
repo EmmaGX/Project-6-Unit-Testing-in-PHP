77709b962bd29fc6adddadd44a869150ddd082cb
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2021-02-16 17:20:41 -0500

    Adds ListingPremiumTest.php

---
+++ tests/ListingPremiumTest.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ .'/../classes/ListingBasic.php';
+
+use PHPUnit\Framework\TestCase;
+
+class ListingPremiumTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function checkToSeeIfGetStatusReturnsPremium()
+    {
+        $data = ['id' => 1, 'title' => 'test'];
+        $listing = new ListingPremium($data);
+        $listing->setStatus('premium');
+        $this->assertEquals('premium', $listing->getStatus(), 'Did not return premium');
+    }
+}
